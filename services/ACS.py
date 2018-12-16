04ddcd80068a9c50e6d64103c87877e52e6cbf0c
s.preslitsky <s.preslitsky@gmail.com>
2018-12-16 22:00:33 +0300

    Add getting user photos from folder

--- services/ACS.py
+++ services/ACS.py
@@ -5,15 +5,18 @@ from models.user import User
 from models.vector import Vector
 from services.service_interfaces import IAccessControlSystem
 from typing import Dict, Tuple, List
+from os import listdir
+from os.path import isfile, join
 
 
 class AccessControlSystem(IAccessControlSystem):
 
-    def __int__(self):
+    def __init__(self):
         FlexServ = win32com.client.Dispatch("FlexServer.FlexServerGlobal")
         token = FlexServ.AuthenticateUser("admin", "admin", False)
         self._FlexACS = FlexServ.GetObject(token, "FlexACSModule.FlexACS")
         self._FlexDB = FlexServ.GetObject(token, "FlexDB.FlexDBModule")
+        self._path = "C:\\Program Files (x86)\\Octagram\\client_temp\\" #TODO: брать из конфига
 
     #TODO: проверка статуса двери
     def open_door(self, door: ControlPoint, user: User): #TODO: добавить отправку события о том, что сотрудник зашел
@@ -23,8 +26,17 @@ class AccessControlSystem(IAccessControlSystem):
         Users = self._FlexDB.GetUsers4Device("S-1-0581B9AD-5CDC-4d86-A328-0D94A615A418") # TODO: брать SID двери из ControlPoint
         return User.key_id in [u.strSID for u in Users]
 
-    def get_user_photo(self, user: User): #TODO: забирать изображения из папки
-        pass
+    def get_user_photo(self, user: User):
+        photos = list(filter(lambda f: isfile(join(self._path, f)) and
+                                       f.endswith(('.jpeg', '.jpg', '.png', '.JPG')) and
+                                       f.startswith(user.key_id),
+                             listdir(self._path)))
+        
+        if len(photos)==0:
+            return None #TODO: исключения
+
+        with open(self._path + photos[0], "rb") as user_photo:
+            return user_photo
 
     def get_unidentified_users(self) -> List[User]:
         return [User(user.strSID, Vector('')) for user in self._FlexDB.GetUsers("", False, "")]

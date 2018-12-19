27884d18971ad3f50d7b3aa96a302b06e062afa3
s.preslitsky <s.preslitsky@gmail.com>
2018-12-20 01:10:25 +0300

    Add authorization unit tests

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -13,55 +13,41 @@ from services.recognizer import Recognizer
 from core.authorizer import Authorizer
 
 
-# class MockRepository(IRepository):
-#     def __init__(self):
-#         pass
-#
-#     def get_users(self):
-#         return [User('1', Vector('1')), User('2', Vector('2')), User('3', Vector('2'))]
-#
-#     def get_cameras(self):
-#         pass
-#
-#     def get_control_points(self):
-#         pass
-#
-#
-# class MockRecognizer(IRecognizer):
-#     def __init__(self):
-#         pass
-#
-#     def extract(self, picture):
-#         return Vector('2')
-#
-#     def compare_vectors(self, user_vector: Vector, extracted_vector: Vector):
-#         if user_vector.value == '2':
-#             return 1
-#         else:
-#             return 0.1
-#
-#
-# class MockAccessControlSystem(IAccessControlSystem):
-#     def __init__(self):
-#         self.opened_for = None
-#
-#     def open_door(self, door: ControlPoint, user: User):
-#         self.opened_door = door
-#         self.opened_for = user
-#
-#     def has_access(self, door: ControlPoint, user: User):
-#         return True
 
+class TestAuthorization(unittest.TestCase):
 
-class TestAuthorizer(unittest.TestCase):
-
-    @patch("services.repository.Repository")
-    @patch("services.recognizer.Recognizer")
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
     @patch("services.service_interfaces.IAccessControlSystem")
-    def test_PermittedUser_AccessGranted(self,
-                                         MockRepository: mock.MagicMock,
-                                         MockRecognizer: mock.MagicMock,
-                                         MockAccessControlSystem: mock.MagicMock):
+    def test_SomePermittedUsers_DoorOpenOnce(self,
+                                             MockRepository: mock.MagicMock,
+                                             MockRecognizer: mock.MagicMock,
+                                             MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+        mock_recognizer.compare_vectors.return_value = 1
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1')),
+                                                  User('2', Vector('2')),
+                                                  User('3', Vector('3'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_called_once()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_PermittedUser_OpenDoor(self,
+                                    MockRepository: mock.MagicMock,
+                                    MockRecognizer: mock.MagicMock,
+                                    MockAccessControlSystem: mock.MagicMock):
         mock_acs = MockAccessControlSystem.return_value
         mock_acs.has_access.return_value = True
 
@@ -78,13 +64,13 @@ class TestAuthorizer(unittest.TestCase):
         authorizer.authorize(None)
         mock_acs.open_door.assert_called()
 
-    @patch("services.repository.Repository")
-    @patch("services.recognizer.Recognizer")
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
     @patch("services.service_interfaces.IAccessControlSystem")
-    def test_NotPermittedUser_AccessDenied(self,
-                                           MockRepository: mock.MagicMock,
-                                           MockRecognizer: mock.MagicMock,
-                                           MockAccessControlSystem: mock.MagicMock):
+    def test_NotPermittedUser_DoNothing(self,
+                                        MockRepository: mock.MagicMock,
+                                        MockRecognizer: mock.MagicMock,
+                                        MockAccessControlSystem: mock.MagicMock):
         mock_acs = MockAccessControlSystem.return_value
         mock_acs.has_access.return_value = False
 
@@ -101,13 +87,13 @@ class TestAuthorizer(unittest.TestCase):
         authorizer.authorize(None)
         mock_acs.open_door.assert_not_called()
 
-    @patch("services.repository.Repository")
-    @patch("services.recognizer.Recognizer")
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
     @patch("services.service_interfaces.IAccessControlSystem")
-    def test_FaceNotRecognized_DoneNothing(self,
-                                           MockRepository: mock.MagicMock,
-                                           MockRecognizer: mock.MagicMock,
-                                           MockAccessControlSystem: mock.MagicMock):
+    def test_FaceNotRecognized_DoNothing(self,
+                                         MockRepository: mock.MagicMock,
+                                         MockRecognizer: mock.MagicMock,
+                                         MockAccessControlSystem: mock.MagicMock):
         mock_acs = MockAccessControlSystem.return_value
         mock_acs.has_access.return_value = True
 
@@ -123,6 +109,193 @@ class TestAuthorizer(unittest.TestCase):
         authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
         authorizer.authorize(None)
         mock_acs.open_door.assert_not_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_NoUsers_DoNothing(self,
+                               MockRepository: mock.MagicMock,
+                               MockRecognizer: mock.MagicMock,
+                               MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+        mock_recognizer.compare_vectors.return_value = 1
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = []
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_not_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_ScoreIsLowerThanThreshold_DoNoting(self,
+                                                MockRepository: mock.MagicMock,
+                                                MockRecognizer: mock.MagicMock,
+                                                MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        mock_recognizer.compare_vectors.return_value = authorizer._threshold - 0.0000000001
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_not_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_ScoreIsEqualToThreshold_OpenDoor(self,
+                                              MockRepository: mock.MagicMock,
+                                              MockRecognizer: mock.MagicMock,
+                                              MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        mock_recognizer.compare_vectors.return_value = authorizer._threshold
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_ScoreIsHigherThanThreshold_DoNoting(self,
+                                                 MockRepository: mock.MagicMock,
+                                                 MockRecognizer: mock.MagicMock,
+                                                 MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        mock_recognizer.compare_vectors.return_value = authorizer._threshold + 0.0000000001
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_VectorIsNone_DoNoting(self,
+                                   MockRepository: mock.MagicMock,
+                                   MockRecognizer: mock.MagicMock,
+                                   MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = None
+        mock_recognizer.compare_vectors.return_value = 1
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_not_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_UsersIsNone_DoNoting(self,
+                                  MockRepository: mock.MagicMock,
+                                  MockRecognizer: mock.MagicMock,
+                                  MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+        mock_recognizer.compare_vectors.return_value = 1
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = None
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_not_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_ScoreIsNone_DoNoting(self,
+                                  MockRepository: mock.MagicMock,
+                                  MockRecognizer: mock.MagicMock,
+                                  MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+        mock_recognizer.compare_vectors.return_value = None
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_not_called()
+
+    @patch("services.repository.IRepository")
+    @patch("services.recognizer.IRecognizer")
+    @patch("services.service_interfaces.IAccessControlSystem")
+    def test_OneScoreIsNone_ContinueAuthorization(self,
+                                                  MockRepository: mock.MagicMock,
+                                                  MockRecognizer: mock.MagicMock,
+                                                  MockAccessControlSystem: mock.MagicMock):
+        mock_acs = MockAccessControlSystem.return_value
+        mock_acs.has_access.return_value = True
+
+        mock_recognizer = MockRecognizer.return_value
+        mock_recognizer.extract.return_value = Vector('1')
+        mock_recognizer.compare_vectors.side_effect = [None, 1]
+
+        mock_repository = MockRepository.return_value
+        mock_repository.get_users.return_value = [User('1', Vector('1')),
+                                                  User('2', Vector('2'))]
+        mock_repository.get_control_points.return_value = [ControlPoint('1', '1')]
+        mock_repository.get_cameras.return_value = [Camera('1', ControlPoint('1', '1'))]
+
+        authorizer = Authorizer(mock_repository, mock_recognizer, mock_acs)
+        authorizer.authorize(None)
+        mock_acs.open_door.assert_called()
 
 
 if __name__ == '__main__':
